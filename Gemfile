c84182775530e700d7d01756bffd5f727719e9dd
阿部司 <destaque1216@gmail.com>
2021-11-18 09:56:07 +0900

    add activerecord

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in check_foreign_key.gemspec
 gemspec
 
+gem "activerecord"
 gem "rake", "~> 13.0"
 
 gem "rspec", "~> 3.0"
--- lib/check_foreign_key.rb
+++ lib/check_foreign_key.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "check_foreign_key/version"
+require "active_record"
 
 module CheckForeignKey
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.check_key
+    ActiveRecord::Base
+  end
 end
