d530d87b0217f5ba6a52223b226f83dd5706b75f
阿部司 <destaque1216@gmail.com>
2021-11-19 10:09:46 +0900

    [wip] fix check_key

--- lib/check_foreign_key.rb
+++ lib/check_foreign_key.rb
@@ -7,6 +7,15 @@ module CheckForeignKey
   class Error < StandardError; end
 
   def self.check_key
-    ActiveRecord::Base
+    foreign_key_tables = []
+    # 存在するテーブルの確認
+    tables = ActiveRecord::Base.connection.tables
+    # 存在するテーブルに対しforeign_keyの設定の有無を確認
+    foreign_key_tables = tables.flat_map do |table|
+      ActiveRecord::Base.connection.foreign_keys(table.intern).map(&:to_table).map(&:classify)
+    end
+
+    # 上記でforeign_keyが設定されているテーブルのモデルのリレーションの内容を確認
+    foreign_key_tables.map(&:constantize).map(&:reflect_on_all_associations).flatten
   end
 end
