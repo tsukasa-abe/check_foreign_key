bcabee0935ed3c3fa90f322cf21b67dbd61d2b93
阿部司 <destaque1216@gmail.com>
2021-12-09 09:57:43 +0900

    fix

--- lib/check_foreign_key.rb
+++ lib/check_foreign_key.rb
@@ -6,8 +6,6 @@ require "active_record"
 module CheckForeignKey
   class Error < StandardError; end
 
-  DEPENDENT_OPTION = "{:dependent=>:destroy}"
-
   class << self
     def check_key
       foreign_key_tables = []
@@ -26,9 +24,10 @@ module CheckForeignKey
       # to_table -> 主キー側のtable
       # from_tables -> 外部キー制約をもったtableのグループ
       foreign_key_groups.each do |to_table, from_tables|
+        # memo: アソシエーションで関連しないdependent optionを定義していた場合下記のconstantizeでクラスを呼び出し際に（ActiveRecord::Associations::Builder::Associationのprivate methodの）check_dependent_optionsでエラーになる
         to_table_associations = to_table.classify.constantize.reflect_on_all_associations
         to_table_associations.each do |to_association|
-          unless from_tables.include?(to_association.name) && to_association.options.to_s.include?(DEPENDENT_OPTION)
+          unless from_tables.include?(to_association.name) && to_association.options.present?
             put_recommended_dependent(to_association, to_table)
           end
         end
@@ -38,7 +37,7 @@ module CheckForeignKey
     private
 
     def put_recommended_dependent(from_table, to_table)
-      p "#{from_table.name}に外部キー制約があるのに#{to_table}側の定義でdependet: :destroyがついてないよ〜"
+      p "#{from_table.name}に外部キー制約ついていますが#{to_table}側の定義でdependetオプションがついていません。大丈夫ですか？"
     end
   end
 end
